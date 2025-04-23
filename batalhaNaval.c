b5324fc036058584a002167b96be68ec81e90358
neuzenir <115902927+neuzenir@users.noreply.github.com>
2025-04-23 21:42:16 +0000

    implementada a entrada das coordenadas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,47 +4,72 @@
 #define COLUNAS 10
 
 int main() {
-    //Declaração e inicialização das variáveis, vetores e matriz
-    int i, j;
-    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tabuleiro [LINHAS][COLUNAS];
 
-    //Preenche o tabuleiro com zeros
+    //Declaração e inicialização das variáveis, vetores e matriz
+    int i, j, tamanho_navio = 3;
+    char coluna[COLUNAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int linha[LINHAS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int tabuleiro [LINHAS][COLUNAS];
+    int navio_horizontal_linha;
+    int navio_horizontal_coluna;
+    int navio_vertical_linha;
+    int navio_vertical_coluna;
+    int navio_diagonal_principal_linha;
+    int navio_diagonal_principal_coluna;
+    int navio_diagonal_secundaria_linha;
+    int navio_diagonal_secundaria_coluna;
+
+    //Preenche o tabuleiro com zeros para representar a água
     for (i = 0; i < LINHAS; i++){
         for (j = 0; j < COLUNAS; j++){
             tabuleiro[i][j] = 0;
         }
     }
 
-     /*Preenche a posição de um navio na horizontal na
-     linha 2, nas colunas F, G e H e outro navio na vertical,
-     na coluna C, linhas 3, 4 e 5*/
-     for (i = 0; i < LINHAS; i++){
-        for (j = 0; j < COLUNAS; j++){
-            tabuleiro[1][5] = 3;
-            tabuleiro[1][6] = 3;
-            tabuleiro[1][7] = 3;
-            tabuleiro[2][2] = 3;
-            tabuleiro[3][2] = 3;
-            tabuleiro[4][2] = 3;
 
+    /*Solicita a entrada das coordenadas, verifica se o navio está dentro dos limites do tabuleiro e
+    preenche a posição de um navio na horizontal*/
+    printf("Entre com as coordenadas iniciais (linha e coluna) \npara posicionar um navio na horizontal: \n");
+    scanf("%d\n %d", &navio_horizontal_linha, &navio_horizontal_coluna);
+    if (navio_horizontal_coluna + tamanho_navio > COLUNAS) {
+        printf("ERRO: Coordenadas inválidas, o navio horizontal \nestá fora dos limites do tabuleiro!\n\n");
+    } else {
+        for (int j = navio_horizontal_coluna; j < navio_horizontal_coluna + tamanho_navio; j++) {
+            tabuleiro[navio_horizontal_linha][j] = 3;
         }
     }
 
-    /*Preenche a posição de um navio na diagonal, se
-    a soma do valor de "i" e de "j" for igual a 8. */
-     for (i = 0; i < 9; i++){
-        for (j = 0; j < 3; j++){
+    /*Solicita a entrada das coordenadas, verifica se o navio está dentro dos limites do tabuleiro e
+    preenche a posição de um navio na vertical*/
+    printf("Entre com as coordenadas iniciais (linha e coluna) \npara posicionar um navio na vertical: \n");
+    scanf("%d\n %d", &navio_vertical_linha, &navio_vertical_coluna);
+    if (navio_vertical_linha + tamanho_navio > LINHAS) {
+        printf("ERRO: Coordenadas inválidas, o navio vertical \nestá fora dos limites do tabuleiro! \n\n");
+    } else {
+        for (int i = navio_vertical_linha; i < navio_vertical_linha + tamanho_navio; i++) {
+            tabuleiro[i][navio_vertical_coluna] = 3;
+        }
+    }
+
+    /*Solicita a entrada das coordenadas, verifica se o navio está dentro dos limites do tabuleiro e
+    preenche a posição de um navio na diagonal secundária, se a soma dos valores de "i" e de "j" for igual a 8*/
+      printf("Entre com as coordenadas iniciais (linha e coluna) \npara posicionar um navio na diagonal secundária: \n");
+    scanf("%d\n %d", &navio_diagonal_secundaria_linha, &navio_diagonal_secundaria_coluna);
+    if (navio_diagonal_secundaria_linha + tamanho_navio > LINHAS || navio_diagonal_secundaria_coluna + tamanho_navio > COLUNAS) {
+        printf("ERRO: Coordenadas inválidas, o navio da diagonal secundária \nestá fora dos limites do tabuleiro! \n\n");
+    } else {
+     for (i = 0; i < 3 + tamanho_navio; i++){
+        for (j = 0; j < 3 + tamanho_navio; j++){
             if (i + j == 8){
             tabuleiro[i][j] = 3;
             }
         }
      }
+     }
 
 
-    /*Preenche a posição de um navio na diagonal, se
-    o valor de "i" for igual ao valor de "j". */
+    /*Preenche a posição de um navio na diagonal principal, se
+    os valores de "i" e "j" forem iguais. */
      for (i = 6; i < 9; i++){
         for (j = 6; j < 9; j++){
             if (i == j){
@@ -57,7 +82,7 @@ int main() {
     printf ("\n :.:.:.:.:. Jogo Batalha Naval .:.:.:.:.: \n\n");
     printf ("   ");
 
-    for (j = 0; j < 10; j++){
+    for (j = 0; j < COLUNAS; j++){
 
         printf ("%c   ", coluna[j]);//Imprime a letra das colunas
     }
